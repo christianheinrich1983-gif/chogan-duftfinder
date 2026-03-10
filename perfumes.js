9f6e5b0cf5195e43caeebd288dff3ffcb1f0ce27
christianheinrich1983-gif <christianheinrich1983@gmail.com>
2026-03-10 19:42:11 +0100

    Update perfumes.js

--- perfumes.js
+++ perfumes.js
@@ -1,14 +1,13 @@
 const perfumes = [
 
-{number:"006",name:"Opium Style"},
-{number:"007",name:"Jadore Style"},
-{number:"010",name:"Alien Style"},
-{number:"011",name:"Light Blue Style"},
-{number:"019",name:"Lady Million Style"},
-{number:"024",name:"Classic No5 Style"},
-{number:"027",name:"Tresor Style"},
-{number:"042",name:"La Vie Est Belle",cat:"herren"},
-{number:"055",name:"Black Opium Style"},
-{number:"071",name:"Allure Style"}
+const perfumes = [
+
+{number:"006",name:"Opium Style",cat:"damen"},
+{number:"007",name:"Jadore Style",cat:"damen"},
+{number:"010",name:"Alien Style",cat:"damen"},
+{number:"042",name:"La Vie Est Belle",cat:"damen"},
+{number:"055",name:"Black Opium",cat:"damen"},
+{number:"094",name:"Fresh Style",cat:"herren"},
+{number:"118",name:"Amber Luxury",cat:"luxury"}
 
 ]

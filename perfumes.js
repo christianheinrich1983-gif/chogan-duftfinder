4c916eaf28617e0e91e728e3071f33d2b4904548
christianheinrich1983-gif <christianheinrich1983@gmail.com>
2026-03-11 17:35:46 +0100

    Update perfumes.js

--- perfumes.js
+++ perfumes.js
@@ -5,49 +5,23 @@ number:"006",
 name:"Opium",
 cat:"damen",
 inspired:"orientalischer Damenklassiker",
-desc:"Ein warmer orientalischer Duft mit Gewürzen, Vanille und Amber. Elegant und sinnlich."
+desc:"Warmer, orientalischer Duft mit Vanille und Gewürzen."
 },
 
 {
 number:"007",
 name:"Jadore",
 cat:"damen",
-inspired:"luxuriöser Blumenduft",
-desc:"Ein eleganter Duft mit Jasmin, Rose und Ylang-Ylang."
+inspired:"blumiger Luxusduft",
+desc:"Elegante Mischung aus Jasmin, Rose und Ylang-Ylang."
 },
 
 {
 number:"010",
 name:"Alien",
 cat:"damen",
-inspired:"mystischer Amber-Duft",
-desc:"Intensiver Duft mit Jasmin, Holznoten und Amber."
-},
-
-{
-number:"042",
-name:"La Vie Est Belle",
-cat:"damen",
-inspired:"süßer Gourmand-Duft",
-desc:"Sehr beliebter Duft mit Vanille, Iris und Praline."
-},
-
-{
-number:"055",
-name:"Black Opium",
-cat:"damen",
-inspired:"süßer Kaffee-Duft",
-desc:"Moderner Duft mit Kaffee, Vanille und weißen Blüten."
-},
-
-{
-number:"094",
-name:"Fresh",
-cat:"herren",
-inspired:"frischer Herrenduft",
-desc:"Frischer Duft mit Zitrusnoten, Ambroxan und Holz."
-},
-
+inspired:"mystischer Jasmin Duft",
+desc
 {
 number:"118",
 name:"Amber Luxury",

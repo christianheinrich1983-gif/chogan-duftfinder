2395aee9ee437b15ab9445cc3e7fbc6aed405952
christianheinrich1983-gif <christianheinrich1983@gmail.com>
2026-03-11 17:14:24 +0100

    Update perfumes.js

--- perfumes.js
+++ perfumes.js
@@ -1,11 +1,59 @@
 const perfumes = [
 
-{number:"006",name:"Opium Style",cat:"damen"},
-{number:"007",name:"Jadore Style",cat:"damen"},
-{number:"010",name:"Alien Style",cat:"damen"},
-{number:"042",name:"La Vie Est Belle",cat:"damen"},
-{number:"055",name:"Black Opium",cat:"damen"},
-{number:"094",name:"Fresh Style",cat:"herren"},
-{number:"118",name:"Amber Luxury",cat:"luxury"}
+{
+number:"006",
+name:"Opium",
+cat:"damen",
+inspired:"orientalischer Damenklassiker",
+desc:"Ein warmer orientalischer Duft mit Gewürzen, Vanille und Amber. Elegant und sinnlich."
+},
+
+{
+number:"007",
+name:"Jadore",
+cat:"damen",
+inspired:"luxuriöser Blumenduft",
+desc:"Ein eleganter Duft mit Jasmin, Rose und Ylang-Ylang."
+},
+
+{
+number:"010",
+name:"Alien",
+cat:"damen",
+inspired:"mystischer Amber-Duft",
+desc:"Intensiver Duft mit Jasmin, Holznoten und Amber."
+},
+
+{
+number:"042",
+name:"La Vie Est Belle",
+cat:"damen",
+inspired:"süßer Gourmand-Duft",
+desc:"Sehr beliebter Duft mit Vanille, Iris und Praline."
+},
+
+{
+number:"055",
+name:"Black Opium",
+cat:"damen",
+inspired:"süßer Kaffee-Duft",
+desc:"Moderner Duft mit Kaffee, Vanille und weißen Blüten."
+},
+
+{
+number:"094",
+name:"Fresh",
+cat:"herren",
+inspired:"frischer Herrenduft",
+desc:"Frischer Duft mit Zitrusnoten, Ambroxan und Holz."
+},
+
+{
+number:"118",
+name:"Amber Luxury",
+cat:"luxury",
+inspired:"luxuriöser Amber-Duft",
+desc:"Warmer Duft mit Amber, Holz und Gewürzen."
+}
 
 ]

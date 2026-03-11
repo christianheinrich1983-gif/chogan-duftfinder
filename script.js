cc0999f094acf337f48635a03ca97b82f53db11b
christianheinrich1983-gif <christianheinrich1983@gmail.com>
2026-03-11 17:34:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,19 +32,4 @@ list.forEach(p=>{
 let img="flacon-men-30ml.jpg"
 
 if(p.cat==="damen"){
-img="flacon-women-30ml.jpg"
-}
-
-if(p.cat==="luxury"){
-img="flacon-black-50ml.jpg"
-}
-
-let div=document.createElement("div")
-
-div.className="card"
-
-div.innerHTML = `
-<img src="${img}">
-<div class="card-title">
-${p.number} – Chogan ${p.name}
-</div>
+img="flacon-women
